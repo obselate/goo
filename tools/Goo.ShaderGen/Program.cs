c27c02c4235dba9ee793ae246c1299812b0739ec
Xaz <zack796@gmail.com>
2026-09-20 14:34:20 -0400

    Restore sampling and evaluate HarfBuzz small-text contrast

--- tools/Goo.ShaderGen/Program.cs
+++ tools/Goo.ShaderGen/Program.cs
@@ -879,7 +879,7 @@ internal static class Program
             new AssemblyPart { Path = HarfBuzzFragmentPath, Sha256 = HarfBuzzFragmentSha256 },
             new AssemblyPart { Path = HarfBuzzDrawFragmentPath, Sha256 = HarfBuzzDrawFragmentSha256 },
             new AssemblyPart { Path = "tools/Goo.ShaderGen/Vendored/HarfBuzz-14.3.1/adapters/clip_chain_text.glsl", Sha256 = "892f76421162819caae0db0d0a29aa6b50e3fbdacfa82926b79d423cb27e0274" },
-            new AssemblyPart { Path = "tools/Goo.ShaderGen/Vendored/HarfBuzz-14.3.1/adapters/hb_gpu_draw.frag.wrapper.glsl", Sha256 = "dc0d334afc1ddedc2a900d9141f5ea818c192c03b09ce235b7563b5fb522a07e" }
+            new AssemblyPart { Path = "tools/Goo.ShaderGen/Vendored/HarfBuzz-14.3.1/adapters/hb_gpu_draw.frag.wrapper.glsl", Sha256 = "f5ae5b4e01ba22ff58b3c94f810fa3e09ed328442921ee107aec8b1f54a06fe5" }
         });
         RequireAssembly(manifest.Assemblies[2], "hb_gpu_paint_fragment", new[]
         {
